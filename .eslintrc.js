0fb4cb68715d249b90deb31e270943b660f03960
wangyang65 <wangyang65@baidu.com>
2020-09-28 23:17:57 +0800

    ci:setup webpack + less loader + eslint
    Change-Id: I3e4f45cc574e9b858cdb2aa4d813fe73171d1448

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
+    parser: '@typescript-eslint/parser',
     extends: [
         '@ecomfe/eslint-config',
+        '@ecomfe/eslint-config/typescript',
+        '@ecomfe/eslint-config/react',
     ],
+    rules: {
+        'no-use-before-define': 'off',
+    }
 };
